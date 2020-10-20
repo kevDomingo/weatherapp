1a0ecfd57e85483c7a8476b69e080d58671032f6
Kevin Domingo <kevdomingo10@gmail.com>
2020-10-19 22:00:13 -0700

    Updates

---
+++ precache-manifest.c18ce1df8a1bffa1ab8479e28b4492e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b6c27b8d0029b4e4bc4b5d2d5c00d7",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "875339367fb98b3a4df2",
+    "url": "/weatherapp/static/css/main.eb67038a.chunk.css"
+  },
+  {
+    "revision": "29f61cb57740322bc81a",
+    "url": "/weatherapp/static/js/2.da080151.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weatherapp/static/js/2.da080151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875339367fb98b3a4df2",
+    "url": "/weatherapp/static/js/main.ad96f465.chunk.js"
+  },
+  {
+    "revision": "8862584b26a768bb4b89",
+    "url": "/weatherapp/static/js/runtime-main.cb4f0870.js"
+  },
+  {
+    "revision": "4214e49baed167369d68f2cf5ed91475",
+    "url": "/weatherapp/static/media/coldweather.4214e49b.png"
+  },
+  {
+    "revision": "5a85354e087bc8644f7a106123c9741f",
+    "url": "/weatherapp/static/media/warmweather.5a85354e.png"
+  }
+]);
